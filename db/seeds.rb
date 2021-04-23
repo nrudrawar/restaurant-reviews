f488d1eedabc362b751f3b938ec9acfa01496bae
Nitin Rudrawar <nitin.rudrawar@gmail.com>
2021-04-23 21:12:50 +0100

    seeded 100 restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+puts 'database is clean'
+
+puts 'Creating Restaurants..'
+100.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    rating: rand(1..5)
+  )
+  puts "restuarant #{restaurant.id} is created"
+end
+
+puts "Done!!!"
